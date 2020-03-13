1ce44bb636ba796d05f205a530e784c5d52f1939
hoilunhung <60016157+hoilunhung@users.noreply.github.com>
2020-03-13 13:26:27 -0700

    Add files via upload

--- RQProject1/src/main/java/com/YNLH/park/dao/mapper/UserMapper.java
+++ RQProject1/src/main/java/com/YNLH/park/dao/mapper/UserMapper.java
@@ -8,6 +8,10 @@ import com.YNLH.park.dao.entity.User;
 @Repository
 public interface UserMapper {
 	public int registerUser(User user);
+	public int insertUser(User user);
+	public List<User> selectAllUser();
+	/* ----- 上面是鹏飞的， 注意registerUser*/
+	public int registerUser(User user);
 	public User findUser(String account) throws Exception;
 	public User findUserById(int uid) throws Exception;
 	public User login(String account, String password) throws Exception;
